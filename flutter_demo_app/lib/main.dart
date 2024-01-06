e7efa87f02e1a1990b3cd8c8ade20f9f002de39e
Rafal Ozog <rafalozog@gmail.com>
2024-01-06 08:38:00 +0000

    Dynamic data loading from API implemented

--- flutter_demo_app/lib/main.dart
+++ flutter_demo_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +22,32 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Article {
+  final String title;
+  final String imageUrl;
+
+  Article({required this.title, required this.imageUrl});
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      title: json['data']['title'] as String,
+      imageUrl: json['data']['thumbnail'] as String,
+    );
+  }
+}
+
+Future<List<Article>> fetchArticles() async {
+  final endpoint = 'https://www.reddit.com/r/Wallstreetbets/top.json?limit=10&t=year';
+  final response = await http.get(Uri.parse(endpoint));
+
+  if (response.statusCode == 200) {
+    List<dynamic> children = json.decode(response.body)['data']['children'];
+    return children.map((data) => Article.fromJson(data)).toList();
+  } else {
+    throw Exception('Failed to load articles');
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -30,15 +58,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  int _counter = 0;
   final List<String> _listItems = ['Item 1', 'Item 2', 'Item 3'];
+  late Future<List<Article>> futureArticles;
 
   void _incrementCounter() {
     setState(() {
       _listItems.add("Item ${(_listItems.length + 1)}");
-      _counter = _listItems.length;
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureArticles = fetchArticles();
   }
 
   @override
@@ -60,12 +92,24 @@ class _MyHomePageState extends State<MyHomePage> {
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             Expanded(
-              child: ListView.builder(
-                itemCount: _listItems.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text(_listItems[index]),
-                  );
+              child: FutureBuilder<List<Article>>(
+                future: futureArticles,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        Article article = snapshot.data![index];
+                        return ListTile(
+                          title: Text(article.title),
+                          leading: Image.network(article.imageUrl),
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+                  return CircularProgressIndicator();
                 },
               ),
             ),
